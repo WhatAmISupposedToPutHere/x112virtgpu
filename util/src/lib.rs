e2975a12cb4f89ecf2aae7fe13a6cfd1c95d84e1
Asahi Lina <lina@asahilina.net>
2024-10-31 00:06:46 +0900

    Remove stray SHM files in fallback mode
    We don't use these paths in the host any more, so just remove the file.
    For the preload, this is done immediately. For the ptrace fallback,
    after replacing the futex storage.

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::fs::File;
+use std::fs::{remove_file, File};
 use std::io::ErrorKind;
 
 pub const SHM_PREFIX: &'static str = "/dev/shm/krshm-";
@@ -7,7 +7,7 @@ pub const SHM_DIR: &'static str = "/dev/shm/";
 
 pub type Result<T> = std::result::Result<T, std::io::Error>;
 
-pub fn create_shm_file() -> Result<([u8; 8], File)> {
+pub fn create_shm_file(remove: bool) -> Result<([u8; 8], File)> {
     let mut rng = rand::thread_rng();
     loop {
         let shmem_name = [0x30u8; 8].map(|x| x + rng.gen_range(0..10));
@@ -22,6 +22,9 @@ pub fn create_shm_file() -> Result<([u8; 8], File)> {
             .open(&shmem_path);
         match result {
             Ok(file) => {
+                if remove {
+                    remove_file(&shmem_path)?;
+                }
                 file.set_len(4)?;
                 return Ok((shmem_name, file));
             }
